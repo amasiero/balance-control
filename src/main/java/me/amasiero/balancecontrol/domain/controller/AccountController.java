14e9ecf94a694c8d9a439f0f86b6f98ad9b62747
Andrey Masiero <andreymasiero@gmail.com>
2023-04-21 10:10:27 +0100

    feat: added controller

---
+++ src/main/java/me/amasiero/balancecontrol/domain/controller/AccountController.java
@@ -0,0 +1,42 @@
+package me.amasiero.balancecontrol.domain.controller;
+
+import lombok.AllArgsConstructor;
+import me.amasiero.balancecontrol.domain.dto.AccountDto;
+import me.amasiero.balancecontrol.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Set;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountDto> create(@Valid @RequestBody AccountDto accountDto) {
+        AccountDto responseAccount = accountService.create(accountDto);
+        return ResponseEntity
+                .created(URI.create("/%d".formatted(responseAccount.getId())))
+                .body(responseAccount);
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<AccountDto>> getAll() {
+        return ResponseEntity.ok(accountService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getById(id));
+    }
+}
